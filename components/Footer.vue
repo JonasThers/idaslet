ac1057cb12ea42a1a9799cfdd231f883ae8a881d
JonasThers <jonasthers1992@gmail.com>
2023-01-29 01:39:15 +0200

    Add emails to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,29 +1,44 @@
 <template>
-    <footer>
-        <h2>Follow Idaslet on social media</h2>
-        <Socials />
-    </footer>
+  <footer class="footer">
+    <h2 class="footer__header">Follow Idaslet on social media</h2>
+    <Socials />
+    <div class="footer__emails">
+      <p>Booking: <a href="mailto:Booking@Idaslet.dk">Booking@Idaslet.dk</a></p>
+      <p>General inquiries: <a href="mailto:Idaslet@Idaslet.dk">Idaslet@Idaslet.dk</a></p>
+    </div>
+  </footer>
 </template>
 
 <script>
-import Socials from './Socials.vue';
+import Socials from "./Socials.vue";
 
 export default {
-    name: "Footer",
-    components: { Socials }
-}
+  name: "Footer",
+  components: { Socials },
+};
 </script>
 
 <style scoped>
-footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding-bottom: 3rem;
+.footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 3rem;
 }
 
-h2 {
+.footer__header {
+  text-transform: capitalize;
+  color: var(--white);
+}
+
+.footer__emails {
     color: var(--white);
+    text-align: center;
+}
+
+.footer__emails a {
+    color: var(--white);
+    text-decoration: none;
 }
 </style>
