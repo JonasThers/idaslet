e358f8c6625b222c13162fff8e4e2f363030de7e
JonasThers <jonasthers1992@gmail.com>
2023-01-29 01:03:35 +0200

    Add scroll effect to header

--- components/Header.vue
+++ components/Header.vue
@@ -1,24 +1,54 @@
 <template>
-    <header class="header">
-        <h1>Idaslet</h1>
-        <Socials />
-    </header>
+  <header class="header" :class="{ 'header--dark': !topOfPage }">
+    <h1 class="header__title" :class="{ 'header__title--white': !topOfPage }">Idaslet</h1>
+    <Socials />
+  </header>
 </template>
 
 <script>
-import Socials from './Socials.vue';
+import Socials from "./Socials.vue";
 
 export default {
-    name: 'Header',
-}
+  name: "Header",
+  data() {
+    return {
+      topOfPage: true,
+    };
+  },
+  beforeMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      if (window.pageYOffset > 0) {
+        if (this.topOfPage) this.topOfPage = false;
+      } else {
+        if (!this.topOfPage) this.topOfPage = true;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
 .header {
-    width: 100%;
-    display: flex;
-    position: fixed;
-    align-items: center;
-    justify-content: space-between;
+  width: 100%;
+  display: flex;
+  position: fixed;
+  align-items: center;
+  justify-content: space-between;
+  transition: all .2s ease-in-out;
+}
+
+.header__title {
+    transition: all .2s ease-in-out;
+}
+
+.header--dark {
+    background: var(--transparent-black);
+}
+
+.header__title--white {
+    color: var(--white);
 }
 </style>
