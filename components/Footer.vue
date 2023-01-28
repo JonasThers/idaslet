fda2ff96d4d89b8c41d29c749e73ed2b67b9ac8e
JonasThers <jonasthers1992@gmail.com>
2023-01-28 22:36:07 +0200

    Add social media links

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,24 @@
 <template>
     <footer>
         <h2>Follow Idaslet on social media</h2>
+        <Socials />
     </footer>
 </template>
 
 <script>
+import Socials from './Socials.vue';
+
 export default {
-    name: 'Footer',
+    name: "Footer",
+    components: { Socials }
 }
 </script>
+
+<style scoped>
+footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+</style>
---
+++ components/Socials.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="socials">
+    <a
+      v-for="(social, index) in socials"
+      :key="index"
+      :href="social.url"
+      class="link"
+      target="_blank"
+    >
+      <img :src="social.img" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Socials",
+  data() {
+    return {
+      socials: [
+        {
+          alt: "Spotify link",
+          url: "https://open.spotify.com/artist/6uTmzUp8K9iXpV2peh3F6St",
+          img: "spotify-icon.png",
+        },
+        {
+          alt: "Facebook link",
+          url: "https://www.facebook.com/Idaslet",
+          img: "facebook-icon.png",
+        },
+        {
+          alt: "Youtube link",
+          url: "https://www.youtube.com/@idaslet8004",
+          img: "youtube-icon.png",
+        },
+        {
+          alt: "Instagram link",
+          url: "https://www.instagram.com/Idaslet_Official/",
+          img: "instagram-icon.png",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.socials {
+    display: flex;
+    align-items: center;
+}
+
+.link {
+    padding: .75rem;
+}
+
+img {
+    max-width: 2rem;
+    width: 100%;
+}
+</style>
